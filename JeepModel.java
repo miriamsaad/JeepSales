c0cfcbde926f45ccd470c46084ebdd9c0da66a61
Miriam Saad <122053370+miriamsaad@users.noreply.github.com>
2023-04-04 02:38:22 -0400

    Add files via upload

---
+++ JeepModel.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author miriamsaad
+ *
+ */
+public enum JeepModel {
+  WRANGLER, GRAND_CHEROKEE, CHEROKEE, COMPASS, RENEGADE, GLADIATOR, WRANGLER_4XE
+
+}
---
+++ OptionType.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+public enum OptionType {
+  DOOR, EXTERIOR, INTERIOR, STORAGE, TOP, WHEEL
+}
