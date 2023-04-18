be4bc0735c32d3e52dd94bc375da27798bf7011d
Miriam Saad <122053370+miriamsaad@users.noreply.github.com>
2023-04-17 23:48:14 -0400

    Add files via upload

--- DefaultJeepSalesService.java
+++ DefaultJeepSalesService.java
@@ -3,7 +3,9 @@
  */
 package com.promineotech.jeep.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.promineotech.jeep.dao.JeepSalesDao;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +18,15 @@ import java.util.List;
 
 public class DefaultJeepSalesService implements JeepSalesService {
   
+ @Autowired
+  private JeepSalesDao jeepSalesDao; 
+  
   @Override
   public List<Jeep> fetchJeeps(JeepModel model, String trim) {
     log.info("The fetchJeeps method was called with model={} and trim={}", 
         model, trim);
   
-return null;
+return jeepSalesDao.fetchJeeps(model,trim);
 
 }
 }
--- FetchJeepTest.java
+++ FetchJeepTest.java
@@ -64,9 +64,12 @@ class FetchJeepTest {
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     
     // And: the actual list returned is the same as the expected list
+    List<Jeep> actual = response.getBody();
     List<Jeep> expected = buildExpected();
-   // System.out.println(expected);
-    assertThat(response.getBody()).isEqualTo(expected);
+    
+    actual.forEach(jeep -> jeep.setModelPK(null));
+    
+    assertThat(actual).isEqualTo(expected);
   }
 
   protected List<Jeep> buildExpected() {
